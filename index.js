0184375557694604c402a4c3354f8b69923aac9a
scalande@yahoo.com <scalande@yahoo.com>
2018-06-19 05:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var code = [];
 
 
 function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var facts = [];
      var newFacts = [];
     var i = 0;
     while (i < facts.length) {
