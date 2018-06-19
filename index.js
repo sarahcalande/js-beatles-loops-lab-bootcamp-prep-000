daa4a07f371238cc30a4a6aa11717b285123ac26
scalande@yahoo.com <scalande@yahoo.com>
2018-06-19 05:40:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var code = [];
 
 
 function johnLennonFacts(){
-  const facts = [];
+  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
      var newFacts = [];
     var i = 0;
     while (i < facts.length) {
