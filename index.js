121db8714de9f3dce4dcee8cb4567fc6d617ba89
scalande@yahoo.com <scalande@yahoo.com>
2018-06-19 05:26:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function theBeatlesPlay(musicians,instruments){
+var code = [];
+  
+  for (var i = 1; i <= 4; i++){
+    return [`${musicians[i-1]} plays ${instruments[i-1]}`, `${musicians[i]} plays ${instruments[i]}`, `${musicians[i+1]} plays ${instruments[i+1]}`, `${musicians[i+2]} plays ${instruments[i+2]}`]
+  }
+  
+}
+
+
+function johnLennonFacts(array){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+
+while(facts){
+  return ["He was the last Beatle to learn to drive!!!",
+  "He was never a vegetarian!!!",
+  "He was a choir boy and boy scout!!!",
+  "He hated the sound of his own voice!!!"]
+}
+}
+
+
+function iLoveTheBeatles(Number){
+ var result= [];
+ var i = 0
+  do {"I love the Beatles!" 
+  result = result * i;
+} while (i < 15);
+
+console.log(result);
+}
